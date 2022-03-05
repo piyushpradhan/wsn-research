9c29b2b36cc22b662211fbc3229f3f624e52ba3f
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2022-03-05 19:38:02 +0530

    initial commit

---
+++ find_forwarder.py
@@ -0,0 +1,22 @@
+## Find forwarder
+# Symbols used
+# Notation Description 
+# N Number of sensors 
+# n Number of node in forward area
+# R Transmission range 
+# rk kth route
+# Tk End-to-end delay of kth route 
+# FZ Forward zone
+# Ek Energy consumption of kth route 
+# Rz Forward zone radius
+# Pc (l), Connectivity probability 
+# Ci A ith chromosome (path)
+# Ed(y) Expected distance 
+# E(Fi) Fitness function for energy consumption
+# E(tcost). Expected computational cost 
+# Etrans Transmission energy
+# E(Etotal) Expected energy consumption
+# Erec Receiving energy
+# φ Density of sensors 
+# PSD Probability of success delivery
+
