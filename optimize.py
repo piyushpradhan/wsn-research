05839378440caa70c7221cc8f03df97ab3f42245
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2022-05-07 15:20:06 +0530

    add remaining function for checking if a node falls withing the forward zone

--- optimize.py
+++ optimize.py
@@ -21,6 +21,8 @@
 # PSD Probability of success delivery
 
 import random
+from wrt_polygon import *
+from forward_zone_coord import *
 
 def check_forward_zone(s, d, r, n, i): 
     """
@@ -29,49 +31,75 @@ def check_forward_zone(s, d, r, n, i):
     pass
 
 def gen_coord(): 
+    """
+    Generate random coordinates which represent
+    the location of sensor nodes scattered around
+    """
     x = random.uniform(0, 9)
     y = random.uniform(0, 9)
     return (int(x), int(y))
 
 def calc_dist(x, y): 
+    """
+    Calculate the distance between two sensor nodes
+    """
     return ((x[0]-y[0])**2 + (x[1]-y[1])**2)**0.5
 
-def find_forwarder(fz, s, d, r, n): 
+def find_forwarder(fz, s, d, r, n, forward_zone): 
+    """
+    Find the forwarder node
+    """
     forwarder_list = []
     forwarder_node = ""
     for i in coord: 
         # add forward zone condition
-        if calc_dist(s, i) <= r and (calc_dist(i, d) < calc_dist(s, d)):
+        # check if i falls in FZ (the remaining condition)
+        if calc_dist(s, i) <= r and (calc_dist(i, d) < calc_dist(s, d)) and is_inside_polygon(forward_zone, i):
             forwarder_list.append(i)
-    if d in forwarder_list:
+    if d in forwarder_list or len(forwarder_list) == 0:
         forwarder_node = d
     else: 
         forwarder_node = random.choice(forwarder_list)
     return forwarder_node
 
-def chromosome_form(s, d, fz, r, n):
-    j = 1
+def chromosome_form(s, d, fz, r, n, forward_zone):
+    """
+    Generate a path for transmission of signal
+    """
     chromosome = []
     chromosome.append(s)
     while s != d: 
-        c2 = find_forwarder(fz, s, d, r, n)
+        c2 = find_forwarder(fz, s, d, r, n, forward_zone)
         chromosome.append(c2)
         s = c2
     return chromosome
 
+def population_form(s, d, fz, r, n, p, forward_zone):
+    """
+    Generate a population of paths
+    """
+    population = []
+    for i in range(p):
+        population.append(chromosome_form(s, d, fz, r, n, forward_zone))
+    return population
+
+
 if __name__ == "__main__": 
     n = 50
     s = [1, 3]
-    d = [7, 9]
+    d = [7, 8]
     r = 2
     coord = []
     while True:
+        if len(coord) == n: 
+            break
         new_coord = gen_coord()
         if new_coord not in coord:
             coord.append(gen_coord())
         else:
             pass
-        if len(coord) == n: 
-            break
-    forwarder = find_forwarder({1,2,3,4,5}, s, d, r, coord)
+    forward_zone = point_of_intersection(r, s, d)
+    forwarder = find_forwarder({1,2,3,4,5}, s, d, r, coord, forward_zone)
     # print(find_forwarder({1,2,3,4,5}, s, d, r, coord))
+    population = population_form(s, d, {1,2,3,4,5}, r, coord, 10, forward_zone)
+
